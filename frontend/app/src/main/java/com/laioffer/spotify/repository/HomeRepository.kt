728bfaf084f32c2d12b41ac895cb832cb4429f88
Kong-Bai-Leo <your-email@example.com>
2026-01-17 11:04:09 -0800

    favorite functions added, player added

--- frontend/app/src/main/java/com/laioffer/spotify/repository/HomeRepository.kt
+++ frontend/app/src/main/java/com/laioffer/spotify/repository/HomeRepository.kt
@@ -5,12 +5,14 @@ import com.laioffer.spotify.datamodel.Section
 import com.laioffer.spotify.network.NetworkApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor(private val networkApi: NetworkApi){
     suspend fun getHomeSection(): List<Section> = withContext(Dispatchers.IO) {
+        delay(3000)
             val responseCall = networkApi.getHomeFeed()
             val response = responseCall.execute()
             val sections = response.body()
